e02ce0c1d4484bc887e6dce982e5e5243a52404c
Dana Mulyana <danabontot@gmail.com>
2022-09-15 13:56:34 +0700

    Strings

---
+++ bin/strings.dart
@@ -0,0 +1,16 @@
+String singleQuote = 'Ini adalah String';
+String doubleQuote = "Ini juga String";
+
+print('"What do you think of Dart?" he asked');
+
+print('"I think it\'s great!" I answered confidently');
+
+print("Windows path: C:\\Program Files\\Dart");
+
+print(r'Dia baru saja membeli komputer seharga $1,000.00');
+
+print('Hi \u2665');
+/*
+  output :
+    Hi ♥
+*/
