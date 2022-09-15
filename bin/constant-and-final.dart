4a54661638408afd0d1db54509520f6ccdea428b
Dana Mulyana <danabontot@gmail.com>
2022-09-15 14:35:42 +0700

    constant dan final

---
+++ bin/constant-and-final.dart
@@ -0,0 +1,22 @@
+// KONSTANT
+
+//otomatis
+const pi = 3.14;
+
+//manual
+const num pitwo = 3.14;
+
+void main() {
+  var radius = 7;
+  print('Luas lingkaran dengan radius $radius = ${calculateCircleArea(radius)}');
+}
+
+num calculateCircleArea(num radius) => pitwo * radius * radius;
+
+//FINAL
+final firstName = stdin.readLineSync();
+final lastName = stdin.readLineSync();
+
+//  lastName = 'Dart'; Tidak bisa mengubah nilai
+
+print('Hello $firstName $lastName');
