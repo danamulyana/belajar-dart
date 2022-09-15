207f77ca4748e6dd9f825376683aa10961ea4fe9
Dana Mulyana <danabontot@gmail.com>
2022-09-15 13:53:05 +0700

    number

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -5,6 +5,6 @@ void main(List<String> arguments) {
   stdout.write('Nama Anda : ');
   String name = stdin.readLineSync()!;
   stdout.write('Usia Anda : ');
-  int age = int.parse(stdin.readLineSync());
+  int age = int.parse(stdin.readLineSync()!);
   print('Halo $name, usia Anda $age tahun.');
 }
