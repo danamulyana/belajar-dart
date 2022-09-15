44cda42aa1b521f16f8bcc1340a98f111f0f00e1
Dana Mulyana <danabontot@gmail.com>
2022-09-15 16:08:29 +0700

    Set

---
+++ bin/set.dart
@@ -0,0 +1,31 @@
+var numberSet = {1, 4, 6};
+Set<int> anotherSet = new Set.from([1, 4, 6, 4, 1]);
+
+print(anotherSet);
+
+// Output: {1, 4, 6}
+
+// untuk menambahkan
+numberSet.add(6);
+numberSet.addAll({2, 2, 3});
+
+// untuk menghapus
+numberSet.remove(3);
+
+//untuk menampilkan
+print(numberSet.elementAt(2));
+
+// Output: 6
+
+//untuk menggabungkan
+var setA = {1, 2, 4, 5};
+var setB = {1, 5, 7};
+
+var union = setA.union(setB);
+var intersection = setA.intersection(setB);
+
+print(union);
+print(intersection);
+
+// union: {1, 2, 4, 5, 7}
+// intersection: {1, 5}
