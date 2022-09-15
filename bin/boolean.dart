fb867d745986d0039a9585649ce08274d9a1da73
Dana Mulyana <danabontot@gmail.com>
2022-09-15 13:58:37 +0700

    boolean

---
+++ bin/boolean.dart
@@ -0,0 +1,14 @@
+bool alwaysTrue = true;
+var alwaysFalse = false;
+var notTrue = !true;
+bool notFalse = !false;
+
+// Tanda ! di atas disebut dengan operator “not” atau “bang”. Operator ini
+// berfungsi untuk menegasikan nilai boolean, sederhananya membalik nilai boolean.
+// Misalnya !true sama saja bernilai false.
+
+if(true) {
+print("It's true");
+} else {
+print("It's False");
+}
