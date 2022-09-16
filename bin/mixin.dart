5f6d7693c04bb70009d4e9459391b23aee3956fc
Dana Mulyana <danabontot@gmail.com>
2022-09-16 10:36:32 +0700

    mixins

---
+++ bin/mixin.dart
@@ -0,0 +1,23 @@
+mixin Flyable {
+  void fly() {
+    print("I'm flying");
+  }
+}
+
+mixin Walkable {
+  void walk() {
+    print("I'm walking");
+  }
+}
+
+mixin Swimmable {
+  void swim() {
+    print("I'm swimming");
+  }
+}
+class Mammal{}
+class Bird{}
+
+class Cat extends Mammal with Walkable { }
+
+class Duck extends Bird with Walkable, Flyable, Swimmable { }
