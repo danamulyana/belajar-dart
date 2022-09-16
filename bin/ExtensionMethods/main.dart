0ec7034ec5149e61f4a3f083a0c57655b8eef56e
Dana Mulyana <danabontot@gmail.com>
2022-09-16 10:45:50 +0700

    Extension Methods

---
+++ bin/ExtensionMethods/main.dart
@@ -0,0 +1,8 @@
+import 'Extensions.dart';
+
+void main(){
+  var unsortedNumbers = [2,5,3,1,4];
+  print(unsortedNumbers);
+  var sortedNumbers = unsortedNumbers.sortAsc();
+  print(sortedNumbers);
+}
