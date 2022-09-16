70aec1fc089954547f85fea87c5bae2731ee14d6
Dana Mulyana <danabontot@gmail.com>
2022-09-16 11:09:55 +0700

    anonymous functions

--- bin/Functional-styles/paradigma.dart
+++ bin/Functional-styles/paradigma.dart
@@ -7,7 +7,7 @@ int sum(int num1,int num2){
 int fibonacci(n){
   if(n <= 0){
     return 0;
-  }else if(n === 1){
+  }else if(n == 1){
     return 1;
   }else{
     return fibonacci(n - 1) + fibonacci(n -2);
